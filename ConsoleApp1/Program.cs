1434737ea815d1565e35794018282df72d2f9f56
Matej <matej.sprogar@gmail.com>
2020-04-10 06:49:38 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+interface IDelaj
+{
+    void delaj();
+}
+
+abstract class Zival : IDelaj
+{
+    public int teza;    // kg
+    public abstract void delaj();
+}
+
+class Tiger : Zival
+{
+    public override void delaj() { Console.WriteLine("tiger"); }
+}
+
+class JezniTiger : Tiger
+{
+    public override void delaj() { Console.WriteLine("jezen"); }
+}
+
+class Program {        
+    static void Main()
+    {
+        Zival z = new JezniTiger();
+        z.delaj();
+        z.teza -= 1;
+    }
+}
+
+
+
