cc53e6b6efd077211814ede6dbbff16136680435
Matej <matej.sprogar@gmail.com>
2020-04-10 09:13:16 +0200

    Generics

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,34 +1,74 @@
 ﻿using System;
+using System.Collections.Generic;
 
 interface IDelaj
 {
     void delaj();
 }
-
-abstract class Zival : IDelaj
-{
-    public int teza;    // kg
-    public abstract void delaj();
-}
-
-class Tiger : Zival
-{
-    public override void delaj() { Console.WriteLine("tiger"); }
-}
-
-class JezniTiger : Tiger
-{
-    public override void delaj() { Console.WriteLine("jezen"); }
-}
-
 class Program {        
     static void Main()
     {
-        Zival z = new JezniTiger();
-        z.delaj();
-        z.teza -= 1;
+        izpis(10, 3);
+        izpis(10.2, 5);
+        izpis(7, "alfa");
+        izpis("keks", "smreka");
+
+        Alfa<int> a = new Alfa<int>(42);
+        a.izpis();
+        a.izpis("keks");
+        a.izpis(42);
+    }
+
+    private static void izpis<T, U>(T v, U u)
+    {
+        Console.WriteLine("({0}; {1})", v.ToString(), u.ToString());
+    }
+
+    // Constraint
+    public T max<T>(T a, T b)
+             where T : IComparable<T>
+    {
+        return a.CompareTo(b) > 0 ? a : b;  // a > b
+    }
+
+    public void nekaj<T>(T a) where T : IDelaj, new()
+    {
+        T t = new T();
+        a.delaj();
+        Console.WriteLine(a);
+    }
+
+    public void nekaj(IDelaj a)
+    {
+        a.delaj();
+        Console.WriteLine(a);
+    }
+
+    public void swap<T>(ref T a, ref T b)
+    {
+        T t = a;
+        a = b;
+        b = t;
     }
 }
 
+class Alfa<T>
+{
+    T podatek;
+
+    public Alfa(T p) { podatek = p; }
+    public void izpis()
+    {
+        Console.WriteLine("[{0}]", podatek);
+    }
+    public void izpis<U>(U u)
+    {
+        Console.WriteLine("[{0}, {1}]", podatek, u);
+    }
+    public void izpis(string t)
+    {
+        Console.WriteLine("({0})", t);
+    }
+}
 
 
