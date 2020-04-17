e3bd968c569514d0f7d3c6a7ed47b82b73bc6e68
Matej <matej.sprogar@gmail.com>
2020-04-17 09:51:23 +0200

    MojSklad

---
+++ ConsoleApp1/mojsklad.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MojSklad<Tip>
+{
+    Tip[] data;
+    int kapaciteta = 0;
+    int st_elementov = 0;
+
+    public MojSklad(int kap = 10)
+    {
+        kapaciteta = kap;
+        data = new Tip[kapaciteta];
+    }
+
+    public void push(Tip kaj) {
+        if (st_elementov >= kapaciteta)
+        {
+            kapaciteta *= 2;
+            Tip[] tmp = new Tip[kapaciteta];
+            for (int i = 0; i < st_elementov; ++i)
+                tmp[i] = data[i];
+            data = tmp;
+        }
+        data[st_elementov++] = kaj;
+    }
+    public Tip pop() {
+        if (st_elementov > 0)
+            return data[--st_elementov];
+
+        return default(Tip); 
+    }
+}
+
+delegate void MojaFunkcija();
+
+class mojsklad {        
+    static void xMain()
+    {
+        var msi = new MojSklad<int>();
+        var mss = new MojSklad<string>();
+        var msf = new MojSklad<MojaFunkcija>();
+
+        msf.push(ConsoleApp2.delegati.prva);
+
+        msi.push(10);
+        msi.push(20);
+        msi.push(30);
+
+        mss.push("abc");
+        mss.push("def");
+    }
+}
+
